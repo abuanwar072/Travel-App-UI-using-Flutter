55c4a5ac193f4db8640a39879bf60c6418cff7c5
Abu Anwar <abuanwar072@gmail.com>
2020-09-09 13:30:53 +0600

    initial

---
+++ android/app/src/main/kotlin/com/example/travel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.travel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF3E4067);
+const kTextColor = Color(0xFF3F4168);
+// const kTextColor = Color(0xFF3A3A3A);
