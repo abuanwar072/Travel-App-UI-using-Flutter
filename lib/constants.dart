874a82abeee1c55fb7fab1f25df9a84e2bb95d3d
Abu Anwar <abuanwar072@gmail.com>
2020-09-11 12:33:11 +0600

    update constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,4 +2,12 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF3E4067);
 const kTextColor = Color(0xFF3F4168);
-// const kTextColor = Color(0xFF3A3A3A);
+const kIconColor = Color(0xFF5E5E5E);
+
+const kDefaultPadding = 20.0;
+
+final kDefualtShadow = BoxShadow(
+  offset: Offset(5, 5),
+  blurRadius: 10,
+  color: Color(0xFFE9E9E9).withOpacity(0.56),
+);
